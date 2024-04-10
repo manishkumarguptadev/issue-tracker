774940293e557a4ae7a60f7840911a433832091d
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-04-10 11:11:28 +0530

    dark mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@
 import { Bug } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ModeToggle } from "./mode-toggle";
 const links = [
   { label: "Dashboard", href: "/" },
   { label: "Issues", href: "/issues" },
@@ -10,7 +11,7 @@ const links = [
 function Header() {
   const currentPath = usePathname();
   return (
-    <header className="sticky top-0 z-10 flex h-16 items-center border-b bg-background px-8">
+    <header className="sticky top-0 z-10 flex h-16 items-center justify-between border-b bg-background px-8">
       <nav className="flex gap-12 text-base font-medium ">
         <Link href="/">
           <Bug className="h-6 w-6" />
@@ -30,6 +31,7 @@ function Header() {
           </Link>
         ))}
       </nav>
+      <ModeToggle />
     </header>
   );
 }
