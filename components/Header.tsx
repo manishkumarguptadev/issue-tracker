0ccbb98d557df59624ef232ab6e09c2f628b9d2e
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-04-10 10:59:44 +0530

    active link styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import { Bug } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 const links = [
   { label: "Dashboard", href: "/" },
   { label: "Issues", href: "/issues" },
 ];
 function Header() {
+  const currentPath = usePathname();
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center border-b bg-background px-8">
       <nav className="flex gap-12 text-base font-medium ">
@@ -16,7 +20,11 @@ function Header() {
           <Link
             key={link.href}
             href={link.href}
-            className="text-muted-foreground transition-colors hover:text-foreground"
+            className={`${
+              link.href === currentPath
+                ? "text-foreground "
+                : "text-muted-foreground "
+            } transition-colors hover:text-foreground`}
           >
             {link.label}
           </Link>
