135165b564461d0e5921ba1c8a3ee587f3b35cfe
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-04-11 23:03:49 +0530

    issues read

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -8,8 +8,10 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import db from "@/prisma/db";
 
-export default function IssuesPage() {
+export default async function IssuesPage() {
+  const issues = await db.issue.findMany();
   return (
     <>
       <div className="mb-4 flex items-center">
@@ -25,17 +27,45 @@ export default function IssuesPage() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell>
-                <div className="font-medium">Liam Johnson</div>
-                <Badge className="sm:hidden">Fulfilled</Badge>
-                <div className="font-medium md:hidden">2023-06-23</div>
-              </TableCell>
-              <TableCell className="hidden sm:table-cell">
-                <Badge className="text-xs">Fulfilled</Badge>
-              </TableCell>
-              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
-            </TableRow>
+            {issues.map((issue) => (
+              <TableRow key={issue.id}>
+                <TableCell>
+                  <div className="font-medium">{issue.title}</div>
+                  <Badge
+                    className={`${
+                      issue.status === "CLOSED"
+                        ? "bg-green-50 text-green-500"
+                        : issue.status === "OPEN"
+                          ? "bg-red-50 text-red-500"
+                          : "bg-violet-50 text-violet-500"
+                    } sm:hidden`}
+                    variant={"outline"}
+                  >
+                    {issue.status}
+                  </Badge>
+                  <div className="font-medium md:hidden">
+                    {issue.createdAt.toDateString()}
+                  </div>
+                </TableCell>
+                <TableCell className="hidden sm:table-cell">
+                  <Badge
+                    className={
+                      issue.status === "CLOSED"
+                        ? "bg-green-50 text-green-500"
+                        : issue.status === "OPEN"
+                          ? "bg-red-50 text-red-500"
+                          : "bg-violet-50 text-violet-500"
+                    }
+                    variant={"outline"}
+                  >
+                    {issue.status}
+                  </Badge>
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  {issue.createdAt.toDateString()}
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </div>
